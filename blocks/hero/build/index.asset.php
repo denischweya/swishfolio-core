8a297098e019b67fae3f73f2ee36d43f69d36f70
denischweya <denischweya@gmail.com>
2026-02-04 20:18:31 +0300

    Refactor social media icons into a shared module for better maintainability and consistency across blocks. Update hero and swish-socials blocks to utilize the new SOCIAL_ICONS structure, enhancing code organization and reducing duplication.

--- blocks/hero/build/index.asset.php
+++ blocks/hero/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a11cb9c2e1923740b566');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '9af61caa8f72b5be9111');
--- blocks/swish-socials/build/index.asset.php
+++ blocks/swish-socials/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '899e7c8bb5614eeb763b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd93c62425f4b5f33165d');
